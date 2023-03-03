bf802e9613ab0576f363ffc7b7061cf1df0657cd
Himanshu Patel <HRPatel2000@Gmail.com>
2023-03-03 12:34:27 -0600

    Initial Draft - 10

--- src/test/java/com/oci/ed/http/test/HttpEmailClientSenderTest.java
+++ src/test/java/com/oci/ed/http/test/HttpEmailClientSenderTest.java
@@ -5,10 +5,13 @@ import com.oracle.bmc.submitemail.model.EmailAddress;
 import com.oracle.bmc.submitemail.model.Recipients;
 import com.oracle.bmc.submitemail.model.SubmitEmailDetails;
 import com.oracle.bmc.submitemail.requests.SubmitEmailRequest;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -109,5 +112,46 @@ class HttpEmailClientSenderTest {
                 .build();
 
         assertEquals(422, httpEmailSender.submitSDKEmail(submitEmailRequest));
+    }
+
+    @Test
+    @DisplayName("Should send Html email via Http Endpoint")
+    void shouldSubmitHTMLEmailViaHttpEndpoint() {
+        String htmlContent = null;
+        try {
+            InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("./BasicForm.html");
+            try {
+                htmlContent = IOUtils.toString(in, StandardCharsets.UTF_8);
+            } finally {
+                IOUtils.closeQuietly(in);
+            }
+        } catch (Exception e) {
+            System.out.println("HttpEmailClientSenderTest@shouldSubmitHTMLEmailViaHttpEndpoint@Error >>> " + e);
+        }
+        System.out.println("HttpEmailClientSenderTest@shouldSubmitHTMLEmailViaHttpEndpoint@HtmlContent >>> " + htmlContent);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        SubmitEmailDetails submitEmailDetails = SubmitEmailDetails.builder()
+                .subject("OCI Email SDK!!!")
+                .compartmentId(COMPARTMENT_ID)
+                .recipients(Recipients.builder()
+                        .to(Arrays.asList(EmailAddress.builder()
+                                .email("Himanshu.R.Patel@Oracle.com")
+                                .name("OCI ED Support")
+                                .build()))
+                        .build())
+                .from(EmailAddress.builder()
+                        .email("abc@test.com")
+                        .name("OCI ED Support")
+                        .build())
+                .bodyText("Knock knock from OCI EMAIL SDK for Http Send API!!!")
+                .bodyHtml(htmlContent)
+                .headerFields(headers)
+                .build();
+        SubmitEmailRequest submitEmailRequest = SubmitEmailRequest.builder()
+                .submitEmailDetails(submitEmailDetails)
+                .build();
+        assertEquals(200, httpEmailSender.submitSDKEmail(submitEmailRequest));
     }
 }
